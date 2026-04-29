b7650a30deb4ce1dc29fa3d9b8fd91ec8c99f99b
BoyznFem <fememokotedi@gmail.com>
2026-04-29 08:28:24 +0200

    Added  HashMap and minor fixes to TableMap and IMap

--- src/EXEcutioners/adts/interfaces/IMap.java
+++ src/EXEcutioners/adts/interfaces/IMap.java
@@ -4,12 +4,11 @@ import java.util.Iterator;
 
 public interface IMap<K, V> {
 	int size();
-	boolean containsKey(K key);
 	boolean isEmpty();
 	V get(K key);
 	V put(K key, V value);
 	V remove(K key);
-	Iterator<K> keySet();
-	Iterator<V> values();
-	Iterator<IEntry<K,V>>entrySet();
+	Iterable<K> keySet();
+	Iterable<V> values();
+	Iterable<IEntry<K,V>>entrySet();
 }
